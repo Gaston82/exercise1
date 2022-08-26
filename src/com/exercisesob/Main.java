ae3b6eea04a0892c33e998b82e706cb40cf146ee
Gaston82 <cabezaartaza@hotmail.com>
2022-08-26 02:20:00 +0200

    first commit

---
+++ src/com/exercisesob/Main.java
@@ -0,0 +1,48 @@
+package com.exercisesob;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte number1 = 1; //1 byte
+        short number2 = 2; //2 byte;
+        int number3 = 3; //4byte
+        long number4 = 4; // 8byte
+
+        //punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        //caracter
+        char caracter1 = 'a';
+
+        //booleanos
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //cadenas de texto
+
+        String name = "Gaston";
+        String lastName = "Artaza";
+
+        //tipos envoltorio
+
+        Integer numero = null;
+        Long numero2 = 2L;
+
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(caracter1);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(name);
+        System.out.println(lastName);
+        System.out.println(numero2);
+        System.out.println(numero);
+    }
+}
